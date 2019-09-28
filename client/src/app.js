f47b2d52efe1ada9024c1ea6412297a0e704ed01
Srebalaji Thirumalai <srebalaji@outlook.com>
2019-09-28 23:34:46 +0530

    Complete top feed

--- client/src/app.js
+++ client/src/app.js
@@ -3,49 +3,105 @@ import ReactDOM from "react-dom";
 const axios = require('axios')
 
 import './style.css';
+import './custom.css';
+
+const getSource = (source) => {
+	switch(source) {
+		case 'hn':
+			return 'https://www.jsonstore.io/aae418df7ce1a10c4012355d4764eec09cc54dc2c084d138d7746eb158814ee2'
+		case 'ph':
+			return 'https://www.jsonstore.io/4bcfd16ed408234c1ad240f0123fb8b8fdbc2e2fe2d04e00f323259f9a4185ac'
+		case 'github':
+			return 'https://www.jsonstore.io/42b21a4a92c6846554e41a527b69da17bcad0ea51191c4dfabf6c1b7386fde08'
+		default:
+			return 'https://www.jsonstore.io/aae418df7ce1a10c4012355d4764eec09cc54dc2c084d138d7746eb158814ee2'
+	}
+}
+
+const request = (source) => {
+	return new Promise((res, rej) => {
+		const url = getSource(source)
+		axios.get(url)
+		.then((response) => {
+			res(response)
+		})
+		.catch((error) => {
+			rej(error)
+		})
+	})
+}
 
 class App extends Component {
 	constructor (props) {
 		super(props)
 		this.state = {
-			data: 'loading data..!'
+			data: 'loading data..!',
+			source: 'hn'
 		}
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleRequest(source) {
+		request(source)
+		.then((response) => {
+			this.setState({
+				data: response.data.result
+			})
+		})
+		.catch((err) => {
+			this.setState({
+				data: 'Error loading data..!'
+			})
+		})
+	}
+
+	handleClick(e) {
+		const target = e.target.getAttribute('data-source-name')
+		this.setState({
+			data: 'loading data..!',
+			source: target
+		})
+		this.handleRequest(target)
 	}
 
 	componentDidMount () {
-		axios.get('https://www.jsonstore.io/aae418df7ce1a10c4012355d4764eec09cc54dc2c084d138d7746eb158814ee2')
-			.then((response) => {
-				this.setState({
-					data: response.data.result
-				})
-			})
-			.catch((error) => {
-				this.setState({
-					data: 'Error loading data..!'
-				})
-			})
+		this.handleRequest(this.state.source)
 	}
 
 	render() {
 		const data = this.state.data
 		let msg = ''
 		if (typeof(data) === 'string') {
-			msg = data
+			msg = <p>{data}</p>
 		} else {
-			msg = data.map((d) => {
+			msg = data.map((d, i) => {
 				return (
-					<div key={d.id} className="p-3 font-mono text-base text-grey-800">
-						<a className="hover:bg-red-200" href={d.url} target="_blank">{d.title}</a>
+					<div key={d.id || d.url} className="pb-4 font-mono text-base text-grey-800 text-left">
+						<a className="hover:bg-red-200" href={d.url} target="_blank" style={{'font-family': 'Fira code, monospace'}}>{d.title}</a>
+						<hr className="mt-3" style={{'border-color': '#feb2b2'}}/>
 					</div>
 				)
 			})
 		}
 		return (
-			<div className="flex flex-col ">
-				<p className="mx-auto text-3xl py-6">Top HN stories sorted by votes</p>
-				<div className="flex flex-col mx-auto">
-					{msg}
+			<div className="flex flex-col mt-12">
+				{/*<p className="mx-auto text-3xl py-6" style={{'font-family': 'Fira code, monospace'}}>Top HN stories sorted by votes</p>*/}
+				<div className="flex">
+					<div className="ml-10 px-8">
+						<p className="text-3xl font-light" style={{color: '#f56565'}}>Top Feed</p>
+						<p className="font-light">Top stories sorted by votes.</p>
+						<hr className="w-1/4 my-3" style={{'border-width': '1.5px'}}/>
+						<div className="">
+							<p className={`py-1 pointer ${this.state.source === 'hn' ? 'active' : ''}`} data-source-name="hn" onClick={this.handleClick}>Hacker news</p>
+							<p className={`py-1 pointer ${this.state.source === 'ph' ? 'active' : ''}`} data-source-name="ph" onClick={this.handleClick}>Product hunt</p>
+							<p className={`py-1 pointer ${this.state.source === 'github' ? 'active' : ''}`} data-source-name="github" onClick={this.handleClick}>Github</p>
+						</div>
+					</div>
+					<div className="flex flex-col mr-auto w-3/5 p-2">
+						{msg}
+					</div>
 				</div>
+				
 			</div>
 		)
 	}
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch')
 const moment = require('moment')
 
+const substring = (word) => {
+ return `${word.substring(0, 73)}${word.substring(0, 73).length < word.length ? '...' : ''}`
+}
+
 const request = async (url) => {
 	const response =  await fetch(url)
 	return response.json()
@@ -66,7 +70,74 @@ dos().then(async (a) => {
     body: JSON.stringify(a)
 	})
 	const data = await res.json()
-	console.log(data)
+	console.log(` HN - ${JSON.stringify(response)}`)
 })
 
-export default dos
+
+
+const productHunt = async () => {
+	const res = await fetch('https://api.producthunt.com/v1/posts', {
+		method: 'GET',
+		headers: {
+			Authorization: 'Bearer -2tf26gLpVVgCx2Bi9OKI2L8hROOD4N8fKYxmEnlcHk',
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Host: 'api.producthunt.com'
+		}
+	})
+	let data = await res.json()
+	
+	data = data.posts.map((a) => {
+		return {title: `${a.name} - ${substring(a.tagline)}`, score: a.votes_count, url: a.redirect_url}
+	})
+	.sort((a, b) => {
+		return a.score > b.score ? -1 : a.score < b.score ? 1 : 0
+	})
+	.slice(0, 10)
+
+	const req = await fetch('https://www.jsonstore.io/4bcfd16ed408234c1ad240f0123fb8b8fdbc2e2fe2d04e00f323259f9a4185ac', {
+		method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+	})
+	const response = await req.json()
+	console.log(` PH - ${JSON.stringify(response)}`)
+	
+}
+
+
+const github = async () => {
+	const res = await fetch('https://github-trending-api.now.sh/', {
+		method: 'GET'
+	})
+	let data = await res.json()
+
+	data = data.map((a) => {
+		return {
+			title: `${a.name} - ${substring(a.description)}`,
+			url: a.url,
+			score: a.stars
+		}
+	})
+	.sort((a, b) => {
+		return a.score > b.score ? -1 : a.score < b.score ? 1 : 0
+	})
+	.slice(0, 10)
+
+	const req = await fetch('https://www.jsonstore.io/42b21a4a92c6846554e41a527b69da17bcad0ea51191c4dfabf6c1b7386fde08', {
+		method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+	})
+	const response = await req.json()
+	console.log(` Gtihub - ${JSON.stringify(response)}`)
+}
+
+productHunt()
+github()
